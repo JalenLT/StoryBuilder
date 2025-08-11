5b8c42bb781821da91a1474b9d2c08531bcd0730
Stefan Seunarine <127210112+JalenLT@users.noreply.github.com>
2025-08-10 21:08:47 -0400

    Beginning development on react flow

---
+++ resources/js/pages/board_view.tsx
@@ -0,0 +1,61 @@
+import { useState, useCallback } from 'react';
+import { ReactFlow, Background, Controls, Panel, applyEdgeChanges, applyNodeChanges, NodeChange, Node, Edge, EdgeChange, addEdge } from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
+
+const initialNodes: Node[] = [
+    {
+        id: 'block:1',
+        position: { x: 100, y: 100 },
+        data: { label: 'Block 1' },
+        type: 'input'
+    },
+    {
+        id: 'block:2',
+        position: { x: 300, y: 200 },
+        data: { label: 'Block 2' },
+    }
+];
+
+const initialEdges: Edge[] = [
+    // {
+    //     id: 'block:1-block:2',
+    //     source: 'block:1',
+    //     target: 'block:2',
+    //     type: 'step',
+    //     label: 'relates to'
+    // }
+];
+
+export default function BoardView(){
+    const [nodes, setNodes] = useState(initialNodes);
+    const [edges, setEdges] = useState(initialEdges);
+
+    const onNodesChange = useCallback(
+        (changes: NodeChange[]) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+        []
+    );
+    const onEdgesChange = useCallback(
+        (changes: EdgeChange[]) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+        []
+    );
+    const onConnect = useCallback(
+        (params: any) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+        []
+    );
+
+    return (
+        <div style={{ height: '100vh', width: '100vw' }}>
+            <ReactFlow
+                nodes={nodes}
+                edges={edges}
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                onConnect={onConnect}
+                fitView>
+                <Background />
+                <Controls />
+                <Panel position='bottom-right'>Inspector</Panel>
+            </ReactFlow>
+        </div>
+    );
+}
--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,12 @@
 
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\StoryController;
 
 Route::get('/', function () {
-    return Inertia::render('welcome');
+    return Inertia::render('board_view');
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    // Route::prefix('stories')->name('stories.')->controller(StoryController::class)->group(function () {
-    //     Route::post('store', 'store')->name('store');
-    //     Route::post('genres/update', 'updateGenres')->name('genres.update');
-    //     Route::post('tags/update', 'updateTags')->name('tags.update');
-
-    //     Route::get('{id}', 'getStory')->name('get');
-    //     Route::get('get-all', 'getCurrentUserStories')->name('get-all');
-    // });
-
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
